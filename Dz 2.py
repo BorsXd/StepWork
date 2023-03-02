63ac38b52c7defa05f20e1615de1da5c2d786441
Aziz <azizeldarly@gmail.com>
2023-03-02 21:48:40 +0400

    Отправляй

---
+++ Dz 2.py
@@ -0,0 +1,5 @@
+name = input("Напишите имя студента: ")
+lastname = input('Напишите Фамилие студента: ')
+days = int(input('Задайте кол-во дней: '))
+print(name, lastname, 'на', days, 'дней стоит', days*40,'манат')
+
