7b00c10134bf50759f420e64338304e4df6a9b06
Aziz <azizeldarly@gmail.com>
2023-03-04 22:26:51 +0400

    Отправляй

--- Dz 2.py
+++
@@ -1,5 +0,0 @@
-name = input("Напишите имя студента: ")
-lastname = input('Напишите Фамилие студента: ')
-days = int(input('Задайте кол-во дней: '))
-print(name, lastname, 'на', days, 'дней стоит', days*40,'манат')
-
--- Dz1.py
+++ Dz1.py
@@ -1,5 +1,11 @@
-name = input('Как тебя зовут? ')
-a = input('Напиши Привет: ')
-if a == ('Привет'):
-    print(name,'Привет как делишки.')
+class Car:
+    def __init__(self, mark, motor, horsestrenght, fuel):
+        self.mark = mark
+        self.motor = motor
+        self.horsestrenght = horsestrenght
+        self.fuel = fuel
+        print('Mark is: ', self.mark)
+        print('Motor is: ', self.motor)
+        print('Horsestrenght is: ', self.horsestrenght)
+        print('Fuel is: ', self.fuel)
 
